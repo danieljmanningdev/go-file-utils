86c7177d1d4625b2f4c27f36fd665df75fce47ca
SympticDevelopment <developer@symptic.co.uk>
2026-08-28 22:50:43 +0100

    feat: add write lines function

---
+++ writeLines.go
@@ -0,0 +1,29 @@
+package gofileutils
+
+import (
+	"bufio"
+	"os"
+)
+
+// WriteLines creates or overwrites a file at the given path, writing each string
+// from the slice as a separate line.
+//
+// Author: Daniel Manning <daniel@danieljmanningdev.com>
+// Created: 2026
+// Last Modified: 28 August 2026
+func WriteLines(f filepath, lines []string) {
+	file, err := os.Create(string(f))
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, line := range lines {
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+	writer.Flush()
+}
