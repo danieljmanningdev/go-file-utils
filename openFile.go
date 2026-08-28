ac41ec24f2740ebbef356f54cb18b8085e204db4
SympticDevelopment <developer@symptic.co.uk>
2026-08-28 22:47:33 +0100

    feat: add openFile function

---
+++ openFile.go
@@ -0,0 +1,17 @@
+package gofileutils
+
+import "os"
+
+// OpenFile opens a file from the given path and returns the file pointer.
+// Panics if the file cannot be opened.
+//
+// Author: Daniel Manning <daniel@danieljmanningdev.com>
+// Created: 2026
+// Last Modified: 28 August 2026
+func OpenFile(f filepath) *os.File {
+	file, err := os.Open(string(f))
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
