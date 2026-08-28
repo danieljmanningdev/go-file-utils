3c6565c6a7d994709f4f42058d792e17b000327a
SympticDevelopment <developer@symptic.co.uk>
2026-08-28 22:47:07 +0100

    chore: add filepath type

---
+++ models.go
@@ -0,0 +1,8 @@
+package gofileutils
+
+// filepath represents a custom string type for file system paths.
+//
+// Author: Daniel Manning <daniel@danieljmanningdev.com>
+// Created: 2026
+// Last Modified: 28 August 2026
+type filepath string
