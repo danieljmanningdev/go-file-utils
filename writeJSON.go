4a79accd691baa957a7c283700a6f0ae891f046b
SympticDevelopment <developer@symptic.co.uk>
2026-08-28 22:51:22 +0100

    feat: add write JSON function

---
+++ writeJSON.go
@@ -0,0 +1,24 @@
+package gofileutils
+
+import (
+	"encoding/json"
+	"os"
+)
+
+// WriteJSON encodes any given data structure into pretty-printed JSON
+// and writes it to a file at the specified path.
+//
+// Author: Daniel Manning <daniel@danieljmanningdev.com>
+// Created: 2026
+// Last Modified: 28 August 2026
+func WriteJSON(f filepath, v interface{}) {
+	data, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(string(f), data, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
